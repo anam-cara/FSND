cc26ece96ac269d8a294106bf9b6a6e70bb7ac4d
anam-cara <patrik.kueng@gmail.com>
2021-08-08 09:30:26 +0200

    auth0 prefix fix

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fsnd-anamcara.eu.auth0.com', // the auth0 domain prefix
+    url: 'fsnd-anamcara.eu', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'QT3zUutSh34RHNYJL7518Kol2j5VI8fE', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
